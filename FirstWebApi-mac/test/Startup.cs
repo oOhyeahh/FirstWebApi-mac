6b36c8240b2c35a775bca7009d087aa1c0542c78
oOhyeahh <oliverye9606@gmail.com>
2018-01-17 10:25:29 +1100

    ready to deplay sql server

--- FirstWebApi-mac/test/Startup.cs
+++ FirstWebApi-mac/test/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.EntityFrameworkCore;
@@ -25,6 +26,10 @@ namespace test
         {
             services.AddMvc();
             services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            //// Add Entity Framework services for SQL Server.
+            //services.AddEntityFrameworkSqlServer()
+                    //.AddDbContext<TodoAppContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:TodoAppDB"]));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,6 +43,22 @@ namespace test
             {
                 app.UseExceptionHandler("/Home/Error");
             }
+
+            //loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            //loggerFactory.AddDebug();
+
+            ////Drop and recreate the database at each run
+            //using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
+            //{
+            //    TodoAppContext ctx = serviceScope.ServiceProvider.GetService<TodoAppContext>();
+
+            //    // Drop & recreate the database at each run (we're not using EF DB migrations)
+            //    ctx.Database.EnsureDeleted();
+            //    ctx.Database.EnsureCreated();
+
+            //   
+            //    ctx.EnsureSeedData();
+            //}
 
             app.UseStaticFiles();
 
